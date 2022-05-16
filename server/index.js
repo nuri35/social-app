d663b4161a1612b0e9f97454fcd0fe6260a91812
Nurettin Şen <nurie487@gmail.com>
2022-05-16 22:48:24 +0300

    Secure false

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(session({
         mongoUrl: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin` } : {
         mongoUrl: `mongodb://127.0.0.1:${process.env.DB_PORT}/${process.env.DB_NAME}` }) ,
     cookie:{
+        secure: false,
         maxAge:1000 * 60 * 60 * 24 
     }//1 gun
 
