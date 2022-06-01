6cec2221fda7b1babc35bc5bc9bcc392a6387a6c
Nurettin Şen <nurie487@gmail.com>
2022-06-01 18:10:53 +0300

    Add router

--- server/src/router/notifyRouter.js
+++ server/src/router/notifyRouter.js
@@ -4,7 +4,10 @@ const router = require("express").Router()
 
 
 router.get("/notifies",notifyController.getNotify)
-router.post("/createNotify",notifyController.getNotify)
+router.post("/createNotify",notifyController.createNotify)
+router.patch("/isReadNotify/:id",notifyController.isReadNotify)
+router.delete('/removeNotify/:id', notifyController.removeNotify)
+
 
 
 module.exports=router
