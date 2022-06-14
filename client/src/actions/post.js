fd27d238006658c1a3936e01fbff156f522c6896
Nurettin Şen <nurie487@gmail.com>
2022-06-14 21:13:07 +0300

    Update

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,92 +1,74 @@
-import * as types from "./types"
-import * as api from "../api/index"
-import axios from "axios"
-import {useState,useEffect}from 'react'
+import * as types from "./types";
+import * as api from "../api/index";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({
+      type: types.CREATE_POST,
+      payload: data,
+    });
+  } catch (err) {
+    console.log("hey burdayım hata var" + err);
+  }
+};
 
+export const fetchSinglePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.fetchSinglePost(id);
 
-export const createPost = (post) => async(dispatch)=>{
+    dispatch({
+      type: types.FETCH_SINGLE_POST,
+      payload: data,
+      withCredentials: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    try{
-        const {data} = await api.createPost(post)
-        dispatch({
-            type:types.CREATE_POST,
-            payload:data
-        })
-      
-       }catch(err){
-           console.log("hey burdayım hata var" + err)
-       }
-    
+export const SearchPost = (query, pageNumber) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  const [userInfo, setuserInfo] = useState();
 
+  useEffect(() => {
+    setBlogs([]);
+  }, [query]);
 
-}
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "GET",
+      url: `/api/search`,
+      params: { q: query, page: pageNumber },
+      withCredentials: true,
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        console.log(res.data);
+        setBlogs((prevBlog) => {
+          return [
+            ...new Set([...prevBlog, ...res.data.searcharticles.map((b) => b)]),
+          ];
+        });
+        setHasMore(res.data.searcharticles.length);
+        setLoading(false);
+        setuserInfo(res.data.userInfo);
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setError(true);
+      });
 
+    return () => cancel();
+  }, [query, pageNumber]);
 
-
-export const fetchSinglePost = (id) => async(dispatch)=>{
-
-    try{
-        const {data} = await api.fetchSinglePost(id)
-      
-        dispatch({
-            type:types.FETCH_SINGLE_POST,
-            payload:data,
-            withCredentials:true
-        })
-       }catch(err){
-           console.log(err)
-       }
-    
-
-
-}
-
-
-
-
-export  const SearchPost =  (query,pageNumber) =>{
-
-
-    const [loading,setLoading] = useState(true)
-    const [error,setError] = useState(false)
-    const [blogs,setBlogs] = useState([])
-    const [hasMore,setHasMore] = useState(false)
-    const [userInfo,setuserInfo] = useState()
-
-    useEffect(()=>{
-        setBlogs([])
-    },[query])
-
-    useEffect(()=>{
-    setLoading(true)
-    setError(false)
-    let cancel 
-   axios({
-       method:"GET",
-       url:`/api/search`,
-       params:{q:query,page:pageNumber},
-       withCredentials:true,
-       cancelToken:new axios.CancelToken(c=>cancel = c)
-
-   }).then(res=>{
-
-       setBlogs(prevBlog =>{
-           return  [...new Set([...prevBlog,...res.data.searcharticles.map(b =>b)])]
-       })
-       setHasMore(res.data.searcharticles.length) 
-       setLoading(false)
-       setuserInfo(res.data.userInfo)
-    
-
-   }).catch(err=>{
-       if(axios.isCancel(err)) return
-       setError(true)
-   })
-
-  return () => cancel()
-  },[query,pageNumber])
-
-return {loading,error,blogs,hasMore,userInfo}
-}
-
+  return { loading, error, blogs, hasMore, userInfo };
+};
